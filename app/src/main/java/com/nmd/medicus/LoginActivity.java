2cad77e05424f7e829e29a56ddc7123f4eef7f4c
Aditya Bhave <aditya.bhave41@gmail.com>
2018-03-05 00:25:23 +0530

    :/

--- app/src/main/java/com/nmd/medicus/LoginActivity.java
+++ app/src/main/java/com/nmd/medicus/LoginActivity.java
@@ -4,18 +4,38 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.auth.ui.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private RadioGroup radioGroup;
     static RadioButton rb;
+    TextView name;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,45 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.clearCheck();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        name = (TextView) findViewById(R.id.username);
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+
+                    name.setText(user.getDisplayName() + "\n" + user.getEmail() + "\n" + user.getUid());
+
+                    // Code to save userdata
+                    Log.v("Userdetails",user.getDisplayName()+" "+user.getEmail());
+
+
+
+
+                }
+                else {
+                    // User is signed out
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(providers)
+                                    .setLogo(R.mipmap.ic_launcher)
+                                    .build(),
+                            0);
+                }
+            }
+        };
+
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -44,6 +103,25 @@ public class LoginActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                //String x = "Signed In as" + user.getDisplayName().toString();
+                //Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show();
+                user = mFirebaseAuth.getCurrentUser();
+                name.setText(user.getDisplayName() + "\n" + user.getEmail() + "\n" + user.getUid());
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     public void login(View view)
